df3e9014a537e147cf63b175457c97635becb84f
Jiri Simell <jiri.simell@gmail.com>
2025-10-20 23:39:37 +0300

    Early skeleton version

--- frontend/src/components/cardContents/ResultsCardContent.jsx
+++ frontend/src/components/cardContents/ResultsCardContent.jsx
@@ -2,10 +2,25 @@ import { useState, useContext } from "react";
 import { WorkflowContext } from "../../context/WorkflowContext";
 
 const ResultsCardContent = () => {
+  const { codebook } = useContext(WorkflowContext);
+  const codes = codebook[0];
+
+  // Moving to the next step should be allowed by default in this step
+  useEffect(() => {
+    setProceedAvailable(true);
+  });
+
   return (
-    <div>
-    </div>
+    codes?.length ? (
+      <ul>
+        {codes.map((code, index) => (
+          <li key={index}>{code}</li>
+        ))}
+      </ul>
+    ) : (
+      <p>No codes yet</p>
+    )
   );
-}
+};
 
 export default ResultsCardContent;
