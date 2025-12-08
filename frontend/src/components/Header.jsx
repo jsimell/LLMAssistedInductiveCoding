22cde5615a386097881ee68e80f5d1ccd806d0b7
Jiri Simell <jiri.simell@gmail.com>
2025-12-09 00:08:18 +0200

    Deleted old jsx files that were converted to tsx

--- frontend/src/components/Header.jsx
+++
@@ -1,7 +0,0 @@
-const Header = () => (
-  <div className="sticky top-0 flex items-center w-full h-fit z-20 p-7 border-b border-onBackground bg-primary text-white text-3xl font-semibold">
-    LLM-Assisted Inductive Coding
-  </div>
-);
-
-export default Header;
